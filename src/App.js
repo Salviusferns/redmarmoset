5f947b05337fb0511a48c956af3f1eec805f1ac5
Salviusferns <40175316@hcuc.ac.uk>
2023-01-24 20:43:09 +0000

    chat progree

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import './App.css';
+import NavBar from './components/Chat/Navbar';
+import ChatBox from './components/Chat/ChatBox';
+import Welcome from './components/Chat/Welcome';
 import Profile from './components/Profile';
 import Login from './components/Login';
 import Feed from './components/Feed';
+import { useState } from 'react';
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div>
       <div className="App">
         <Profile/>
         <Login/>
+        
       </div>
       <div className='App-2'>
-        <Feed/>
+        <NavBar/>
+        {!user ? (
+        <Welcome />
+      ) : (
+        <>
+          <ChatBox />
+        </>
+      )}
+      <Feed/>
       </div>
     </div>
   );
---
+++ src/components/Chat/Welcome.js
@@ -0,0 +1,29 @@
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import React from "react";
+import GoogleSignin from "./img/k.png";
+
+const Welcome = () => {
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  return (
+    <main className="welcome">
+      <h2>Welcome to React Chat.</h2>
+      
+      <p>Sign in with Google to chat with with your fellow React Developers.</p>
+      <button className="sign-in">
+        <img
+          onClick={googleSignIn}
+          src={GoogleSignin}
+          alt="sign in with google"
+          type="button"
+        />
+      </button>
+    </main>
+  );
+};
+
+export default Welcome;
